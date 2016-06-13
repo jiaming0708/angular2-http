230d76b9043f77b44c7f0dafadb5d442384398b8
Jimmy <jiaming0708@gmail.com>
2016-06-13 09:28:34 +0800

    practice pipe and event sequence

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,5 +1,6 @@
-import { Component }       from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
 import { AppService } from './app.service';
+import { ExponentialStrengthPipe } from './exponential-strength.pipe';
 
 @Component({
   selector: 'my-app',
@@ -8,21 +9,56 @@ import { AppService } from './app.service';
     <button (click)="getCurrentTime()">get current time</button>
     <p>{{getData}}</p>
     <input [(ngModel)]="Lot" placeholder="please input!" (ngModelChange)="changeLot()">
-    <p>{{Lot}}</p>
+    <p>{{Lot | exponentialStrength: 10}}</p>
   `,
   directives: [
   ],
   providers: [
     AppService
   ],
+  pipes:[
+    ExponentialStrengthPipe
+  ]
 })
 
-export class AppComponent{
+export class AppComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
   constructor(private appService: AppService){}
 
   title = 'Tour of Heroes';
   getData: string;
   Lot: string;
+
+  ngOnInit(){
+    console.log('init');
+  }
+
+  ngOnChanges(){
+    console.log('change');
+  }
+
+  ngDoCheck(){
+    console.log('check');
+  }
+
+  ngOnDestroy(){
+    console.log('destroy');
+  }
+
+  ngAfterContentInit(){
+    console.log('after content init');
+  }
+
+  ngAfterContentChecked(){
+    console.log('after content checked');
+  }
+
+  ngAfterViewInit(){
+    console.log('after view init');
+  }
+
+  ngAfterViewChecked(){
+    console.log('after view checked');
+  }
 
   getCurrentTime(){
     this.appService.getCurrentTime()
